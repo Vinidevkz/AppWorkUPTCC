3e34891fb900bc17f47ad6197574f2f4b875f374
Vinidevkz <149980701+Vinidevkz@users.noreply.github.com>
2024-09-27 19:04:33 -0300

    att

--- src/ApisUrls/apisurls.js
+++ src/ApisUrls/apisurls.js
@@ -1,22 +1,22 @@
 // ApisUrls.js
 const ApisUrls = {
   //login
-  apiNgrok: 'http://bf08-200-53-197-8.ngrok-free.app/api/usuario/login',
+  apiNgrok: 'http://d1f7-200-53-197-8.ngrok-free.app/api/usuario/login',
   apiEmulador: 'http://10.0.2.2:8000/api/usuario/login',
-  apiNgrokUsuario: 'http://bf08-200-53-197-8.ngrok-free.app/api/usuario/',
+  apiNgrokUsuario: 'http://d1f7-200-53-197-8.ngrok-free.app/api/usuario/',
   apiEmuladorUsuario: 'http://10.0.2.2:8000/api/usuario/',
   apiNgrokAlterar: '',
   //cadastro
-  apiNgrokCad: 'http://bf08-200-53-197-8.ngrok-free.app/api/usuario',
+  apiNgrokCad: 'http://d1f7-200-53-197-8.ngrok-free.app/api/usuario',
   apiEmuladorCad: 'http://10.0.2.2:8000/api/usuario',
   //pegar vagas
-    apiNgrokVaga: 'http://bf08-200-53-197-8.ngrok-free.app/api/vaga',
+    apiNgrokVaga: 'http://d1f7-200-53-197-8.ngrok-free.app/api/vaga',
     apiEmuladorVaga: 'http://10.0.2.2:8000/api/vaga/',
   //pegar area de interesse
-  apiNgrokArea: 'http://bf08-200-53-197-8.ngrok-free.app/api/areavaga',
+  apiNgrokArea: 'http://d1f7-200-53-197-8.ngrok-free.app/api/areavaga',
   apiEmuladorArea: 'http://10.0.2.2:8000/api/areavaga',
   //pegar id
-  apiNgrokId: 'http://bf08-200-53-197-8.ngrok-free.app/api/usuario/',
+  apiNgrokId: 'http://d1f7-200-53-197-8.ngrok-free.app/api/usuario/',
   apiEmuladorId: 'http://10.0.2.2:8000/api/usuario/',
   };
   
--- src/pages/initialPages/signin.js
+++ src/pages/initialPages/signin.js
@@ -35,7 +35,7 @@ export default function SignIN({ navigation }) {
     }
 
     try {
-      const response = await fetch(apiEmulador, {
+      const response = await fetch(apiNgrok, {
         method: "POST",
         headers: {
           Accept: "application/json",
