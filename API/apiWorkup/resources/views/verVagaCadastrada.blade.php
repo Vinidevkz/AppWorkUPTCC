aff10e3bf69a23f1c4f4f0a1a0c0aec676ccd7e0
Eduardoofelipe <eduardo.ademir@hotmail.com>
2024-11-07 11:58:07 -0300

    ajuste final

--- API/apiWorkup/resources/views/verVagaCadastrada.blade.php
+++ API/apiWorkup/resources/views/verVagaCadastrada.blade.php
@@ -37,6 +37,9 @@
                             Visualizar perfil
                         </button>
 
+                        {{ $candidato->status->tipoStatusVaga }}
+
+
                         <!-- Modal -->
                         <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel"
                             aria-hidden="true">
