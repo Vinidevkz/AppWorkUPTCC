75bbff61e460f1275da713c5db7d970887156ea7
Vinidevkz <149980701+Vinidevkz@users.noreply.github.com>
2024-11-08 10:21:51 -0300

    att

--- src/ApisUrls/apisurls.js
+++ src/ApisUrls/apisurls.js
@@ -1,92 +1,96 @@
 // ApisUrls.js
 const ApisUrls = {
   //login
-  apiNgrok: "http://254a-2804-388-c334-e500-cd2e-568b-b880-690b.ngrok-free.app/api/usuario/login",
+  apiNgrok: "http://a2b7-200-53-195-156.ngrok-free.app/api/usuario/login",
   apiEmulador: "http://10.0.2.2:8000/api/usuario/login",
-  apiNgrokUsuario: "http://254a-2804-388-c334-e500-cd2e-568b-b880-690b.ngrok-free.app/api/usuario/",
+  apiNgrokUsuario: "http://a2b7-200-53-195-156.ngrok-free.app/api/usuario/",
   apiEmuladorUsuario: "http://10.0.2.2:8000/api/usuario/",
   apiNgrokAlterar: "",
   //cadastro
-  apiNgrokCad: "http://254a-2804-388-c334-e500-cd2e-568b-b880-690b.ngrok-free.app/api/usuario",
+  apiNgrokCad: "http://a2b7-200-53-195-156.ngrok-free.app/api/usuario",
   apiEmuladorCad: "http://10.0.2.2:8000/api/usuario",
   //pegar vagas
-  apiNgrokVaga: "http://254a-2804-388-c334-e500-cd2e-568b-b880-690b.ngrok-free.app/api/vagaApp/",
+  apiNgrokVaga: "http://a2b7-200-53-195-156.ngrok-free.app/api/vagaApp/",
   apiEmuladorVaga: "http://10.0.2.2:8000/api/vagaApp/",
   //pegar area de interesse
-  apiNgrokArea: "http://254a-2804-388-c334-e500-cd2e-568b-b880-690b.ngrok-free.app/api/areavaga",
+  apiNgrokArea: "http://a2b7-200-53-195-156.ngrok-free.app/api/areavaga",
   apiEmuladorArea: "http://10.0.2.2:8000/api/areavaga",
   //pegar id
-  apiNgrokId: "http://254a-2804-388-c334-e500-cd2e-568b-b880-690b.ngrok-free.app/api/usuario/",
+  apiNgrokId: "http://a2b7-200-53-195-156.ngrok-free.app/api/usuario/",
   apiEmuladorId: "http://10.0.2.2:8000/api/usuario/",
 
   //pesquisa
   apiEmuladorVagaPesquisa: "http://10.0.2.2:8000/api/vaga/busca/",
   apiNgrokVagaPesquisa:
-    "http://254a-2804-388-c334-e500-cd2e-568b-b880-690b.ngrok-free.app/api/vaga/busca/",
+    "http://a2b7-200-53-195-156.ngrok-free.app/api/vaga/busca/",
 
   //usuario se candidatar
   apiEmuladorUsuarioVaga: "http://10.0.2.2:8000/api/vagaUsuario",
   apiNgrokUsuarioVaga:
-    "http://254a-2804-388-c334-e500-cd2e-568b-b880-690b.ngrok-free.app/api/vagaUsuario",
-  apiNgrokVerificarCandidatura: "http://254a-2804-388-c334-e500-cd2e-568b-b880-690b.ngrok-free.app/api/verificarCandidatura/",
+    "http://a2b7-200-53-195-156.ngrok-free.app/api/vagaUsuario",
+  apiNgrokVerificarCandidatura: "http://a2b7-200-53-195-156.ngrok-free.app/api/verificarCandidatura/",
   apiEmuladorVerificarCandidatura: "http://10.0.2.2:8000/api/verificarCandidatura/",
   
   apiEmuladorUsuarioVagaCancelar: "http://10.0.2.2:8000/api/vagaUsuario",
   apiNgrokUsuarioVagaCancelar:
-    "http://254a-2804-388-c334-e500-cd2e-568b-b880-690b.ngrok-free.app/api/vagaUsuario",
+    "http://a2b7-200-53-195-156.ngrok-free.app/api/vagaUsuario",
 
     
   //Salvar, cancelar e pegar salvamento da vaga
 
   apiEmuladorSalvarVaga: "http://10.0.2.2:8000/api/salvarVaga/",
   apiNgrokSalvarVaga:
-    "http://254a-2804-388-c334-e500-cd2e-568b-b880-690b.ngrok-free.app/api/salvarVaga/",
+    "http://a2b7-200-53-195-156.ngrok-free.app/api/salvarVaga/",
 
     apiEmuladorCancelSalvarVaga: "http://10.0.2.2:8000/api/salvarVaga/",
     apiNgrokCancelSalvarVaga:
-      "http://254a-2804-388-c334-e500-cd2e-568b-b880-690b.ngrok-free.app/api/salvarVaga/",
+      "http://a2b7-200-53-195-156.ngrok-free.app/api/salvarVaga/",
 
     apiEmuladorPegarVagasSalvas: "http://10.0.2.2:8000/api/salvarVaga",
     apiNgrokPegarVagasSalvas:
-        "http://254a-2804-388-c334-e500-cd2e-568b-b880-690b.ngrok-free.app/api/salvarVaga",
+        "http://a2b7-200-53-195-156.ngrok-free.app/api/salvarVaga",
 
-    apiNgrokVerificarSalvarVaga: "http://254a-2804-388-c334-e500-cd2e-568b-b880-690b.ngrok-free.app/api/verificarSalvarVaga",
+    apiNgrokVerificarSalvarVaga: "http://a2b7-200-53-195-156.ngrok-free.app/api/verificarSalvarVaga",
 
 
     //Urls Empresa
-    apiNgrokEmpresa: "http://254a-2804-388-c334-e500-cd2e-568b-b880-690b.ngrok-free.app/api/showempresa/",
+    apiNgrokEmpresa: "http://a2b7-200-53-195-156.ngrok-free.app/api/showempresa/",
     apiEmuladorEmpresa: "http://10.0.2.2:8000/api/showempresa/",
 
     //Urls Linguas
     apiEmuladorLinguas: "http://10.0.2.2:8000/api/linguas",
-    apiNgrokLinguas: "http://254a-2804-388-c334-e500-cd2e-568b-b880-690b.ngrok-free.app/api/linguas",
+    apiNgrokLinguas: "http://a2b7-200-53-195-156.ngrok-free.app/api/linguas",
 
     //Urls Escolas
     apiEmuladorEscolas: "http://10.0.2.2:8000/api/escolas",
-    apiNgrokEscolas: "http://254a-2804-388-c334-e500-cd2e-568b-b880-690b.ngrok-free.app/api/escolas",
+    apiNgrokEscolas: "http://a2b7-200-53-195-156.ngrok-free.app/api/escolas",
 
     //Urls Denuncias
-    apiNgrokDenunciarVaga: "http://254a-2804-388-c334-e500-cd2e-568b-b880-690b.ngrok-free.app/api/denunciarVaga",
+    apiNgrokDenunciarVaga: "http://a2b7-200-53-195-156.ngrok-free.app/api/denunciarVaga",
     apiEmuladorDenunciarVaga: "http://10.0.2.2:8000/api/denunciarVaga",
-    apiNgrokDenunciarEmpresa: "http://254a-2804-388-c334-e500-cd2e-568b-b880-690b.ngrok-free.app/api/denunciaempresa",
+    apiNgrokDenunciarEmpresa: "http://a2b7-200-53-195-156.ngrok-free.app/api/denunciaempresa",
     apiEmuladorDenunciarEmpresa: "http://10.0.2.2:8000/api/denunciaempresa",
 
     //Urls ver candidaturas do usuario
-    apiNgrokMinhasVagas: "http://254a-2804-388-c334-e500-cd2e-568b-b880-690b.ngrok-free.app/api/minhasvagas",
+    apiNgrokMinhasVagas: "http://a2b7-200-53-195-156.ngrok-free.app/api/minhasvagas",
     apiEmuladorMinhasVagas: "http://10.0.2.2:8000/api/minhasvagas",
 
     //Urls ver vaga pro id da empresa
-    apiNgrokVagaEmpresa: 'http://254a-2804-388-c334-e500-cd2e-568b-b880-690b.ngrok-free.app/api/showvagaempresa/',
+    apiNgrokVagaEmpresa: 'http://a2b7-200-53-195-156.ngrok-free.app/api/showvagaempresa/',
     apiEmuladorVagaEmpresa: 'http://10.0.2.2:8000/api/showvagaempresa/',
 
     //Urls ver vaga baseado na área de interesse
     apiEmuladorVagaPorArea: 'http://10.0.2.2:8000/api/vagaporarea',
-    apiNgrokVagaPorArea: 'http://254a-2804-388-c334-e500-cd2e-568b-b880-690b.ngrok-free.app/api/vagaporarea',
+    apiNgrokVagaPorArea: 'http://a2b7-200-53-195-156.ngrok-free.app/api/vagaporarea',
     
 
     //Urls ver vaga baseado na área de interesse
     apiEmuladorOutrasVagas: 'http://10.0.2.2:8000/api/outrasvagas',
-    apiNgrokOutrasVagas: 'http://254a-2804-388-c334-e500-cd2e-568b-b880-690b.ngrok-free.app/api/outrasvagas'
+    apiNgrokOutrasVagas: 'http://a2b7-200-53-195-156.ngrok-free.app/api/outrasvagas',
+
+    //Urls Notificacoes
+    apiNgrokNotificacoes: 'http://a2b7-200-53-195-156.ngrok-free.app/api/notificacoes',
+    apiEmuladorNotificacoes: 'http://10.0.2.2:8000/api/notificacoes',
 };
 
 export default ApisUrls;
--- src/pages/notifications.js
+++ src/pages/notifications.js
@@ -1,12 +1,40 @@
 import React from 'react';
 import { View, Text, StyleSheet, SafeAreaView, ScrollView } from 'react-native';
+import { Context } from "../pages/initialPages/context/provider";
 import { useTheme } from "../pages/initialPages/context/themecontext";
-import { useState } from 'react';
+import { useState, useContext, useEffect } from 'react';
+import ApisUrls from "../ApisUrls/apisurls.js";
+import axios from 'axios';
 
 import styles from '../styles/notifications.js'
 
 export default function Notifications() {
   const { theme, toggleTheme } = useTheme({Notifications});
+  const { userId } = useContext(Context);
+  const [notifications, setNotifications] = useState([])
+
+  const { apiNgrokNotificacoes } = ApisUrls;
+
+  useEffect(() => {
+    const fetchNotifications = async () => {
+      try {
+        const response = await axios.get(`${apiNgrokNotificacoes}/${userId}`);
+        setNotifications(response.data);
+        console.log(notifications);
+        console.log(`${apiNgrokNotificacoes}/${userId}`)
+      } catch (erro) {
+        console.log(erro);
+      }
+    };
+
+    fetchNotifications(); // Primeira requisição imediata
+
+    const intervaloId = setInterval(fetchNotifications, 2000); // Requisições a cada 2 segundos
+
+    return () => clearInterval(intervaloId); // Limpa o intervalo ao desmontar o componente
+  }, []);
+
+
   return (
     <SafeAreaView>
       <View style={[styles.containerTop, {backgroundColor: theme.backgroundColorNavBar}]}>
@@ -16,6 +44,7 @@ export default function Notifications() {
         <Text style={[styles.DMSansRegular, {color: theme.textColor}]}>Nenhuma notificação</Text>
       </View>
     </SafeAreaView>
+
   );
 }
 
